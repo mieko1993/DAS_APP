0c3eb7e4a613718f0cf02ac4b42a81db509b5ba4
mieko nagatome <mieko_193@hotmail.com>
2015-08-02 15:07:24 -0500

    Loaded forget activity

--- app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Forget2.java
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Forget2.java
@@ -6,19 +6,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
 
-public class Forget extends ActionBarActivity {
+public class Forget2 extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget);
+        setContentView(R.layout.activity_forget2);
+    }
+
+
+
+
+
+
+
+    //when button Back is clicked
+    public void onClickButtonBackForget(View view){
+        super.onBackPressed();
     }
 
     //When the textView SEND is clicked
-    public void onClickButtonSend(View view){
-        Intent intent=new Intent(Forget.this,Forget2.class);
+    public void onClickButtonLoginForget(View view){
+        Intent intent=new Intent(Forget2.this,Login.class);
         startActivity(intent);
     }
-
-
 }
