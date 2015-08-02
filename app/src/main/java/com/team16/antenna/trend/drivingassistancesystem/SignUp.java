1af0d8a05a683d2c3bd86c8ebf0dfc894f3e78a8
mieko nagatome <mieko_193@hotmail.com>
2015-08-02 13:35:36 -0500

    Loaded sign up activity review 2

--- app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SignUp.java
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SignUp.java
@@ -2,6 +2,7 @@ package com.team16.antenna.trend.drivingassistancesystem;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 
 public class SignUp extends ActionBarActivity {
@@ -11,5 +12,8 @@ public class SignUp extends ActionBarActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_sign_up);
         }
+    //when button DONE is clicked
+    public void onClickButtonDone(View view){
+        super.onBackPressed();
     }
-
+}
